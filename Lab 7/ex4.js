8b4545562071202b615ae8392733bc509602c16b
Jonovanl <59938494+Jonovanl@users.noreply.github.com>
2020-02-27 12:48:52 -1000

    master

---
+++ Lab 7/ex4.js
@@ -0,0 +1,10 @@
+name =  'Jonovan';
+
+likesBeer = true;
+
+console.log(name);
+
+if ( !likesBeer) {console.log('beer is something I dislike');}
+else{console.log('I like beer!');}
+
+ 
---
+++ new.js
@@ -0,0 +1,6 @@
+var age = 21;
+var count = 0;
+while(count <= age){
+    console.log(++count)
+    
+}
