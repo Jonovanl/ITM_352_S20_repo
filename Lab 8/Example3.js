f8611f85bb95c69802872715c67de6e56d30a9df
Jonovanl <59938494+Jonovanl@users.noreply.github.com>
2020-03-02 22:30:53 -1000

    master

---
+++ Lab 8/Example3.js
@@ -0,0 +1,6 @@
+var age = 21;
+
+for(var count = 1; count < age; count++){
+    console.log(count);
+    
+}
--- new.js
+++
@@ -1,6 +0,0 @@
-var age = 21;
-var count = 0;
-while(count <= age){
-    console.log(++count)
-    
-}
