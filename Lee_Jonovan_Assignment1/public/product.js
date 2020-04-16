088858ac7981ddd151af56109c136fbd87f90e02
Jonovanl <59938494+Jonovanl@users.noreply.github.com>
2020-04-16 12:13:23 -1000

    master

--- Lee_Jonovan_Assignment1/public/product.js
+++ Lee_Jonovan_Assignment1/public/product.js
@@ -2,32 +2,32 @@ var products =
 [
    {
        "product":"Strawberry Cake",
-       "Price":"40.00",
+       "Price":40.00,
        "image":"./images/strawberryshortcake.jpg",
    }, 
    {
         "product":"Brownies",
-        "Price":"20.00",
+        "Price":20.00,
         "image":"./images/brownies.jpg",
 }, 
 {
         "product":"Pies",
-        "Price":"30.00",
+        "Price":30.00,
         "image":"./images/pies.jpg",
 }, 
 {
         "product":"Ice Cream Cake",
-        "Price":"35.00",
+        "Price":35.00,
         "image":"./images/icecreamcake.jpg",
 }, 
 {
         "product":"Donuts",
-        "Price":"2.00",
+        "Price":2.00,
         "image":"./images/donuts.jpg",
 }, 
 {
         "product":"Malasadas",
-        "Price":"1.50",
+        "Price":1.50,
         "image":"./images/malasadas.jpg",
 }, 
 
